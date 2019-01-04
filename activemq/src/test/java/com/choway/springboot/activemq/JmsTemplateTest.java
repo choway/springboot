d1d43a5fcd1c80a9ea2d5fa68897444c1755f0b8
choway <354988859@qq.com>
2019-01-04 17:29:21 +0800

    add jmsTemplate.

---
+++ activemq/src/test/java/com/choway/springboot/activemq/JmsTemplateTest.java
@@ -0,0 +1,48 @@
+package com.choway.springboot.activemq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.jms.support.JmsUtils;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.util.Random;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmsTemplateTest {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+
+	@Test
+	public void sendMsg() {
+		jmsTemplate.send("queue.test", new MessageCreator() {
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("test msg : " + new Random().nextInt(100));
+			}
+		});
+	}
+
+	@Test
+	public void receiveMsg() {
+		try {
+			TextMessage message = (TextMessage) jmsTemplate.receive("queue.test");
+			String text = message.getText();
+			System.out.println(text);
+		} catch (JMSException e) {
+			throw JmsUtils.convertJmsAccessException(e);
+		}
+	}
+
+}
