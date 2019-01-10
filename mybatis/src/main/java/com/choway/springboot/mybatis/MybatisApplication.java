6f11a8d6606ddd6a92f01c46d6f73c0c997a1b91
choway <354988859@qq.com>
2019-01-10 13:10:57 +0800

    add mybatis junit test.

--- mybatis/src/main/java/com/choway/springboot/mybatis/MybatisApplication.java
+++ mybatis/src/main/java/com/choway/springboot/mybatis/MybatisApplication.java
@@ -1,9 +1,11 @@
 package com.choway.springboot.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.choway.springboot.mybatis.dao")
 public class MybatisApplication {
 
 	public static void main(String[] args) {
--- mybatis/src/test/java/com/choway/springboot/mybatis/MybatisApplicationTests.java
+++ mybatis/src/test/java/com/choway/springboot/mybatis/MybatisApplicationTests.java
@@ -1,17 +1,30 @@
 package com.choway.springboot.mybatis;
 
+import com.choway.springboot.mybatis.dao.UserMapper;
+import com.choway.springboot.mybatis.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.Random;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MybatisApplicationTests {
 
+	@Autowired
+	private UserMapper userMapper;
+
 	@Test
-	public void contextLoads() {
+	public void test_mybatis() {
+		User user = new User();
+		user.setName("test"+new Random().nextInt(100));
+		user.setCreateTime(new Date());
+		userMapper.insert(user);
 	}
 
 }
-
