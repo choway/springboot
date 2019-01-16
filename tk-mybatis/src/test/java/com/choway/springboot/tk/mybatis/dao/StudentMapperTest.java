accd50175d4e0e1e4dd59ece6daeaba402af95aa
choway <354988859@qq.com>
2019-01-16 17:47:56 +0800

    add mybatis page helper.

--- tk-mybatis/src/test/java/com/choway/springboot/tk/mybatis/dao/StudentMapperTest.java
+++ tk-mybatis/src/test/java/com/choway/springboot/tk/mybatis/dao/StudentMapperTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
 
 
