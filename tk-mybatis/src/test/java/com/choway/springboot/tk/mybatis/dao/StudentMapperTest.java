0172c6b75d0b82c7576ae86b39354ba15ae5fca6
choway <354988859@qq.com>
2019-01-16 17:47:23 +0800

    add mybatis page helper.

--- tk-mybatis/src/test/java/com/choway/springboot/tk/mybatis/dao/StudentMapperTest.java
+++ tk-mybatis/src/test/java/com/choway/springboot/tk/mybatis/dao/StudentMapperTest.java
@@ -1,6 +1,10 @@
 package com.choway.springboot.tk.mybatis.dao;
 
 import com.choway.springboot.tk.mybatis.model.Student;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.RowBounds;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentMapperTest {
@@ -38,6 +44,27 @@ public class StudentMapperTest {
 		Student student = studentMapper.selectByPrimaryKey(2);
 		student.setName("student" + new Random().nextInt(100));
 		studentMapper.updateByPrimaryKey(student);
+	}
+
+	@Test
+	public void test_page_by_row_bounds() {
+		int page = 2;
+		int size = 5;
+		int offset = (page-1)*size;
+
+		studentMapper.selectByRowBounds(null, new RowBounds(offset, 5));
+	}
+
+	@Test
+	public void test_page_helper() {
+		int page = 2;
+		int size = 5;
+		Page<Student> studentPage = PageHelper.startPage(page, size);
+
+		log.info("student page = {}", studentPage);
+		log.info("current page num = {}", studentPage.getPageNum());
+
+		studentMapper.selectAll();
 	}
 
 }
